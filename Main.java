65ec1c5b82433d92ce8fb1b16f24e943146da132
EsikabuluDaniel <esikabulu2680@student.babcock.edu.ng>
2020-09-26 15:40:48 +0000

    Varibles ongnmentAssi

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
+    int days not paid=20,int Daily Payment=1000,int Amount loaned;
+    int days paid, int amount left to pay, days not paid, Amount paid=5000, Amount not paid in a day;
+    Amount loaned= Days not paid * Daily payment;
+    System.out.printIn(Amount Loaned);
+    Days paid= Amount paid/Daily payment;
+    System.out.printIn(Days paid);
+    Amount not paid in a Day= Amount paid % Daily payment;
+    System.out.printIn(Amount not paid in a day);
+    Amount left to pay=((Days not paid - Days paid)*Daily payment)*(Daily payment - Amount not paid in a day);
+    System.out.printIn(Amount left to pay);
    // kindly use appropriate data types for your declaration
    // declare variables days the debtor defaulted, amount the debtor is to pay per day, amount the debtor is to pay and assign values to them
    
